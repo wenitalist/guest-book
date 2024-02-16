54df0c09cd1f4ab6d054686a84a1311bb6652e87
wenitalist <wenitalist@gmail.com>
2024-02-16 16:28:59 +0300

    init repo

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+?>
+
+<DOCTYPE html>
+<html lang="ru">
+    <header>
+        <meta charset="UTF-8">
+        <title>Главная страница</title>
+    </header>
+    <main>
+        
+    </main>
+</html>

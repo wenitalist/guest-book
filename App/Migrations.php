67d5223debf2270c03ee08ffac176ba0bcca604e
wenitalist <wenitalist@gmail.com>
2024-03-06 13:33:48 +0300

    create table in constructor

--- App/Migrations.php
+++ App/Migrations.php
@@ -12,6 +12,12 @@ class Migrations {
     {
         $database = Connect::getInstance();
         $this->connect = $database->getConnection();
+
+        $this->connect->exec("CREATE TABLE IF NOT EXISTS migrations_accounting (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            status varchar(30) NOT NULL
+        )");
     }
 
     public function make(string $name): string {
